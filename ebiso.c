2453ceb02ae92d7cb340dd320cc161b94fa291cb
gozora <c@gozora.sk>
2015-09-14 16:38:32 +0200

    ebiso.c was forgotten to commit ;-)

--- ebiso.c
+++ ebiso.c
@@ -26,24 +26,18 @@
  */
 
 #include "ebiso.h"
-#include <dirent.h>
-#include <errno.h>
-#include <unistd.h>
-#include <getopt.h>
-#include <ctype.h>
 
 int main(int argc, char *argv[]) {
+   struct file_list_t *list = (struct file_list_t*) malloc(sizeof(struct file_list_t));
+   struct file_list_t *rr_list = list;
    struct ISO_data_t ISO_data;
    FILE *fp = NULL;
    FILE *boot_catalog = NULL;
-   struct file_list_t *list = (struct file_list_t*) malloc(sizeof(struct file_list_t));
-   struct file_list_t *rr_list = list;
+   void *boot_descriptor = (void *) malloc(BLOCK_SIZE);
    void *header = NULL;
    void *terminator = NULL;
-   void *path_table = NULL;
-   void *boot_descriptor = (void *) malloc(BLOCK_SIZE);
-   uint32_t path_table_size = 0;
-   char iso_file[MAX_DIR_STR_LEN];
+   void *path_table_LSB = NULL;
+   void *path_table_MSB = NULL;
    int option = 0;
    int e_flag = 0;
    int o_flag = 0;
@@ -52,15 +46,15 @@ int main(int argc, char *argv[]) {
    memset(list, 0, sizeof(struct file_list_t));
    memset(boot_descriptor, 0, BLOCK_SIZE);
    memset(&ISO_data, 0, sizeof(struct ISO_data_t));
-   memset(iso_file, 0, MAX_DIR_STR_LEN);
    
    /*
-    * Root directory
+    * Artificially create root directory record
     */
    strncpy(list->name_path, ".", 1);
    list->name_short_len = 1;
    list->size = 4096;
    list->st_mode = 16877;
+   list->mtime = time(NULL);
    list->dir_id = 1;
    list->next = (struct file_list_t*) malloc(sizeof(struct file_list_t));
    list->parent_id = 1;
@@ -105,7 +99,7 @@ int main(int argc, char *argv[]) {
                goto cleanup;
             }
             else {
-               strncpy(iso_file, optarg, sizeof(iso_file));
+               strncpy(ISO_data.iso_file, optarg, sizeof(ISO_data.iso_file));
                o_flag++;
             }
          break;
@@ -118,6 +112,7 @@ int main(int argc, char *argv[]) {
    
    /*
     * First syntax check
+    * I dont line argc != 6 this will be chnaged later ;-)
     */
    if (e_flag != 1 || o_flag != 1 || argc != 6) {
       msg(MSG_SYNTAX);
@@ -127,40 +122,34 @@ int main(int argc, char *argv[]) {
 
    /* Working dir will be the last argument */
    strncpy(ISO_data.work_dir, argv[argc-1], MAX_DIR_STR_LEN);
+   
+   /* Yes, BOOT.CAT file will stay in (/) root of ISO file */
    snprintf(ISO_data.boot_cat_file, MAX_DIR_STR_LEN, "%s/%s", ISO_data.work_dir, "BOOT.CAT");
    snprintf(ISO_data.efi_boot_file_full, MAX_DIR_STR_LEN, "%s/%s", ISO_data.work_dir, ISO_data.efi_boot_file);
    
-   /*
-    * Check if we can read working dir
-    */
+   /* Check if we can read working dir */
    if ((rv = check_availability(ISO_data.work_dir, TYPE_DIR, MODE_READ)) != 0)
       goto cleanup;
    
-   /*
-    * Check if we can read efiboot.img 
-    */
+   /* Check if we can read UEFI boot image */
    if ((rv = check_availability(ISO_data.efi_boot_file_full, TYPE_FILE, MODE_READ)) != 0)
       goto cleanup;
    
    /*
-    * Try to create dest iso file
-    * This will truncate existing file (if any)
+    * Check if destination directory is writeable.
+    * This will as well truncate existing file (if any) to zero
     */
-   if ((fp = fopen(iso_file, "w")) == NULL) {
-      printf("Error: Failed to create output file [%s]: %s\n", iso_file, strerror(errno));
+   if ((fp = fopen(ISO_data.iso_file, "w")) == NULL) {
+      printf("Error: Failed to create output file [%s]: %s\n", ISO_data.iso_file, strerror(errno));
       goto cleanup;
    }
    else
       fclose(fp);
    
-   /*
-    * Delete dest iso file (if any)
-    */
-   unlink(iso_file);
+   /* Delete dest iso file (if any) */
+   unlink(ISO_data.iso_file);
    
-   /* 
-    * Create boot calalog (only as placeholder)
-    */
+   /* Create boot calalog (only as placeholder) */
    if ((boot_catalog = fopen(ISO_data.boot_cat_file, "w")) != NULL) {
       fwrite(boot_descriptor, 1, BLOCK_SIZE, boot_catalog);
       fclose(boot_catalog);
@@ -171,62 +160,53 @@ int main(int argc, char *argv[]) {
       goto cleanup;
    }
    
-   /*
-    * Create initial file structure either from current dir (no argument was given)
-    * or from user given argument
-    */
-   if ((rv = fill(ISO_data.work_dir, &rr_list)) != 0)
+   /* Create initial file structure */
+   if ((rv = list_create(ISO_data.work_dir, &rr_list)) != E_OK)
       goto cleanup;
    
-   /*
-    * Assign future LBAs
-    */
-   if (iso9660_assign_LBA(list, &ISO_data) == -1) {
-      rv = E_NOTFOUND;
-      goto cleanup;
-   }
+   /* Calculate path table offset, so LBAs can be offsetted in future */
+   ISO_data.path_table_offset = get_path_table_offset(list);
+   
+   /* Assign future LBAs to created file list */
+   iso9660_assign_LBA(list, &ISO_data);
       
-   /*
-    * Write data to boot catalog
-    */
-   et_boot_catalog(ISO_data, ISO_data.work_dir);
+   /* Write data to boot catalog */
+   if ((rv = et_boot_catalog(ISO_data)) != E_OK)
+      goto cleanup;
    
    /*
     * Create path table and assamble header
+    * ISO_data in iso9660_path_table() is used to store path table size
     */
-   path_table_size = iso9660_path_table(list, &path_table);
-   iso9660_header(path_table_size, &header, *list, ISO_data.LBA_last);
+   if ((rv = iso9660_path_table(list, &path_table_LSB, LSB, &ISO_data)) != E_OK)
+      goto cleanup;
+   else {
+      iso9660_path_table(list, &path_table_MSB, MSB, NULL);
+      iso9660_header(&header, *list, ISO_data);
+   }
    
-   /*
-    * Assamble terminator
-    */
+   /* Assamble terminator */
    iso9660_terminator(&terminator);
    
-   /*
-    * Assamble boot descriptor
-    */
+   /* Assamble boot descriptor */
    et_boot_record_descr(&boot_descriptor, ISO_data);
    
-   /*
-    * Write all to file
-    */
-   fp = fopen(iso_file, "w");
-   fseek(fp, 0x8000, SEEK_SET);                                // Starting block in file
-   fwrite(header, BLOCK_SIZE, 1, fp);                          // 0x8000
-   fwrite(boot_descriptor, BLOCK_SIZE, 1, fp);                 // 0x8800
-   fwrite(terminator, BLOCK_SIZE, 1, fp);                      // 0x9000
-   fseek(fp, BLOCK_SIZE, SEEK_CUR);                            // 0x9800
+   /* Write meta data to file */
+   fp = fopen(ISO_data.iso_file, "w");
+   fseek(fp, 0x8000, SEEK_SET);                                                     // Starting block in file
+   fwrite(header, BLOCK_SIZE, 1, fp);                                               // 0x8000 - LBA 0x10 - Static
+   fwrite(boot_descriptor, BLOCK_SIZE, 1, fp);                                      // 0x8800 - LBA 0x11 - Static
+   fwrite(terminator, BLOCK_SIZE, 1, fp);                                           // 0x9000 - LBA 0x12 - Static
+   fseek(fp, BLOCK_SIZE, SEEK_CUR);                                                 // 0x9800 - LBA 0x13 - Static
    
-   fwrite(path_table, BLOCK_SIZE, 1, fp);                      // 0xA000 - needs to be adjusted for larger path tables ...
-   fseek(fp, BLOCK_SIZE, SEEK_CUR);                            // 0xA800 - one block empty
-   fseek(fp, BLOCK_SIZE, SEEK_CUR);                            // 0xB000 - BE path table will be here
-   fseek(fp, BLOCK_SIZE, SEEK_CUR);                            // 0xB800 - one block empty
+   fwrite(path_table_LSB, (ISO_data.path_table_offset + 1) * BLOCK_SIZE, 1, fp);    // (0xA000 - LBA 0x14) - Dynamic
+   fseek(fp, BLOCK_SIZE, SEEK_CUR);                                                 // (0xA800 - LBA 0x15)
+   fwrite(path_table_MSB, (ISO_data.path_table_offset + 1) * BLOCK_SIZE, 1, fp);    // (0xB000 - LBA 0x16)
+   fseek(fp, BLOCK_SIZE, SEEK_CUR);                                                 // (0xB800 - LBA 0x17)
    
-   /*
-    * Write of files failed, get rid of iso_file               
-    */
-   if ((rv = iso9660_directory(list, fp)) != OK)               // 0xC000
-      unlink(iso_file);
+   /* Write of files failed, get rid of output iso file */
+   if ((rv = iso9660_directory(list, fp)) != E_OK)                                  // 0xC000
+      unlink(ISO_data.iso_file);
 
    fclose(fp);
    
@@ -234,7 +214,7 @@ int main(int argc, char *argv[]) {
    int i = 0;
    switch (DEBUG) {
       case 1:
-         printf("%-5s %-4s %-40s %-11s %-3s %-7s %-5s\n", "Level", "PID", "Name", "Size", "ID", "LBA", "Flag");
+         printf("%-5s %-4s %-50s %-11s %-3s %-7s %-5s %-7s %-30s\n", "Level", "PID", "Name", "Size", "ID", "LBA", "Flag", "Blocks", "Date");
          for (i = 0; i <= 8; i++)
             disp_level(list, i);
       break;
@@ -243,9 +223,10 @@ int main(int argc, char *argv[]) {
 
 cleanup:
    
-   clean_list(list);
+   list_clean(list);
    free(terminator);
-   free(path_table);
+   free(path_table_LSB);
+   free(path_table_MSB);
    free(header);
    free(boot_descriptor);
    
@@ -255,6 +236,8 @@ cleanup:
 #ifdef DEBUG
 static void disp_level(struct file_list_t *list_to_display, int level) {
    char flag = 0;
+   struct tm *ts;
+   char buff[80];
    
    while(list_to_display->next != NULL) {
       if (list_to_display->level == level) {
@@ -262,30 +245,18 @@ static void disp_level(struct file_list_t *list_to_display, int level) {
             flag = 'D';
          else
             flag = 'F';
-      
-         printf("%-5d %-4d %-40s %-11d %-3d %-7d %-5c\n", level, list_to_display->parent_id, list_to_display->name_path, list_to_display->size, \
-         list_to_display->dir_id, list_to_display->LBA, flag);
+         
+         ts = localtime(&list_to_display->mtime);
+         strftime(buff, sizeof(buff), "%a %Y-%m-%d %H:%M:%S %Z", ts);
+         
+         printf("%-5d %-4d %-50s %-11d %-3d %-7d %-5c %-7d %-30s\n", level, list_to_display->parent_id, list_to_display->name_path, list_to_display->size, \
+         list_to_display->dir_id, list_to_display->LBA, flag, list_to_display->blocks, buff);
       }
       
       list_to_display = list_to_display->next;
    }
 }
 #endif
-
-static void clean_list(struct file_list_t *list_to_clean) {
-   struct file_list_t *curr = NULL;
-   struct file_list_t *head = NULL;
-   
-   curr = list_to_clean->next;
-
-   while (curr != NULL) {
-      head = curr->next;
-      free(curr);
-      curr = head;
-   }
-   
-   free(list_to_clean);
-}
 
 static int check_availability(char *filename, enum check_type_l type, enum check_mode_l mode) {
    struct stat rr_stat;
@@ -352,4 +323,26 @@ Arguments:\n\
       printf("%s: Bad syntax\n", PROGNAME);
       printf("Try '%s --help' for more information.\n", PROGNAME);
    }
+}
+
+static uint32_t get_path_table_offset(struct file_list_t *file_list) {
+   int pad_len = 0;
+   uint32_t path_table_size = 0;
+   
+   while(file_list->next != NULL ) {
+      if (S_ISDIR(file_list->st_mode)) {
+         if (file_list->name_short_len % 2 != 0)
+            pad_len = 1;
+         else
+            pad_len = 0;
+         
+         
+         path_table_size += 8 + file_list->name_short_len + pad_len;
+      }
+      
+      file_list = file_list->next;
+   }
+   
+   return path_table_size / (BLOCK_SIZE + 1);
+   
 }
